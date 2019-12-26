6edfcac00f46d6f7522b49c03ae2de2a596b128e
Rob Simpson <rsimpson@ventera.com>
2019-12-26 11:11:53 -0500

    Setting up Amplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import config from './config';
+import {Amplify} from 'aws-amplify';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'notes-app-client';
+
+  ngOnInit() {
+   Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    Storage: {
+      region: config.s3.REGION,
+      bucket: config.s3.BUCKET,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID
+    },
+    API: {
+      endpoints: [
+        {
+          name: 'notes',
+          endpoint: config.apiGateway.URL,
+          region: config.apiGateway.REGION
+        }
+      ]
+    }
+   });
+  }
 }
---
+++ src/app/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-rob"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://24x35vkief.execute-api.us-east-1.amazonaws.com"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_QmOdqhkrC",
+    APP_CLIENT_ID: "67q56m3kn14p6v9e1v7fftp0sg",
+    IDENTITY_POOL_ID: "us-east-1:ddcc19f6-4b1a-458f-99f0-dc87fc50fb9a"
+  }
+};
